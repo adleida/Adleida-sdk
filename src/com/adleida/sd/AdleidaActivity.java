e82af861487e5f366a35469a6dacdd861631fbc0
xiaojian <finally_xiaojian@163.com>
2015-06-16 19:09:21 +0800

    add request code

--- src/com/adleida/sd/AdleidaActivity.java
+++ src/com/adleida/sd/AdleidaActivity.java
@@ -1,6 +1,11 @@
 package com.adleida.sd;
 
+import java.util.List;
+import java.util.UUID;
+
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
@@ -10,26 +15,28 @@ import android.webkit.WebView;
 
 /**
  * 
- * @author jcle
- * this is ad sdk
+ * @author jcle this is ad sdk
  * 
  */
 public class AdleidaActivity {
 
 	private static final String address = "http://123.57.70.242/js/ad.js";
 	private static Context context;
+
 	public static Context getContext() {
 		return context;
 	};
+
 	public static View createView(Context context) {
-		
+
 		WebView localView = new WebView(context);
-		//设置web宽度自适应，高度三百
-		localView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,300));
+		// 设置web宽度自适应，高度三百
+		localView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+				300));
 		// getwindowsManager
 		WindowManager windows = (WindowManager) context
 				.getSystemService(Context.WINDOW_SERVICE);
-		//获取屏幕宽高
+		// 获取屏幕宽高
 		DisplayMetrics dm = new DisplayMetrics();
 		windows.getDefaultDisplay().getMetrics(dm);
 		int height = dm.heightPixels; // 获取手机屏幕高度
@@ -41,13 +48,20 @@ public class AdleidaActivity {
 		String model = android.os.Build.MODEL; // 获取手机型号
 		String brand = android.os.Build.BRAND; // 获取手机信息
 		String version = android.os.Build.VERSION.RELEASE;// 获取手机版本号
+		UUID uuid = UUID.randomUUID(); // random device_id
+		String country = context.getResources().getConfiguration().locale
+				.getCountry(); // resources is CN
 		String requestJSONString = "{    'adunit': {        'type': 1,        'floor': 1.6,        'cid': '1101201',        'param': {            'count': 3        }     },    'device': {        'os': 'ANDROID',        'os_version': "
 				+ version
 				+ ",        'resolution_w':"
 				+ width
 				+ " ,        'resolution_h': "
 				+ height
-				+ ",        'network_id': 70120,        'device_type': 2,        'device_id': '6C0590AFF652982CEB7B3F7D8214C508',        'device_id_enc': 1,        'delected_language': 'zh_CN',        'brand': "
+				+ ",        'network_id': 70120,        'device_type': 2,        'device_id': "
+				+ uuid
+				+ ",        'device_id_enc': 1,        'delected_language': "
+				+ country
+				+ ",        'brand': "
 				+ brand
 				+ ",        'model': "
 				+ model
@@ -57,8 +71,8 @@ public class AdleidaActivity {
 				+ "</script>\n"
 				+ "<script type='text/javascript' language='javascript' src="
 				+ address + "></script>\n</body>\n</html>";
+		Log.v("requestdata", requestJSONString);
 		localView.loadData(html, "text/html", "utf-8");
 		return localView;
 	}
-
 }
